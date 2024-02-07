04c5713035024fff57185f49e2f90627455b2c24
iomars-X230 <iomars@163.com>
2024-02-07 15:44:44 +0800

    updated

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 make LDEXPORT="-static -s" clean
-make LDEXPORT="-static -s" -j4
+make LDEXPORT="-static -s" CONFIG_LTO=y CONFIG_WIN32=y CONFIG_WIN32=y -j4
 
 zip -9 -r quickjs-$(cat version)-win$(echo ${MSYSTEM:0-2}).zip qjs.exe run-test262.exe
-mkdir ./bin
+mkdir -p ./bin
 mv qjs.exe qjsc.exe run-test262.exe ./bin
 mkdir -p ./lib/quickjs
 cp libquickjs.a ./lib/quickjs/libquickjs.d.a
